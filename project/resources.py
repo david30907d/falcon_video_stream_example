ccd7d1f76eefb16ccfb64863700559237bccab39
davidtnfsh <davidtnfsh@gmail.com>
2020-08-22 02:29:45 -0900

    <fix>: specify using piCamera

--- project/resources.py
+++ project/resources.py
@@ -5,7 +5,6 @@ import cv2
 import falcon
 from imutils.video import VideoStream
 
-
 class HTMLResource:
     """
     Use this resource to get HTML
@@ -24,16 +23,17 @@ class VideoResource:
     """
 
     def on_get(self, _, resp):
-        labeled_frame = self._get_frame(VideoStream(src=0).start())
+        labeled_frame = self._get_frame(VideoStream(src=0, usePiCamera=True).start())
         resp.content_type = "multipart/x-mixed-replace; boundary=frame"
-        resp.status = falcon.HTTP_200
         resp.stream = labeled_frame
 
-    def _get_frame(self, camera, fps=10000):
+    def _get_frame(self, camera, fps=100000):
         fps_count = 0
+        import time
+        time.sleep(2)
         while True:
             if fps_count % fps == 0:
-                image = camera.get_frame()
+                image = camera.read()
                 _, jpeg = cv2.imencode(".jpg", image)
                 yield (
                     b"--frame\r\n"
